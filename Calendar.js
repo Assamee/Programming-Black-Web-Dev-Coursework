9c19d415a52f8b7da6a7330d66437661aa84def1
As-Samee Clayton <assameeclayton12@gmail.com>
2025-12-19 22:35:39 +0000

    Used DOM to access user Inputs in JS

--- Calendar.js
+++ Calendar.js
@@ -1,4 +1,30 @@
-function AddEvent() {
-    
+// Accessing the users inputs after the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+    // Access HTML Elements
+    const titleInput = document.getElementById("TitleInput");
+    const descriptionInput = document.getElementById("DescriptionInput");
+    const LocationInput = document.getElementById("LocationInput");
+    const startdateInput = document.getElementById("StartDateInput");
+    const enddateInput = document.getElementById("EndDateInput");
+    const saveButton = document.getElementById("SaveEventButton");
 
-}
+    // Event Listener for Save Button
+    saveButton.addEventListener("click", () => {
+        const title = titleInput.value;
+        const description = descriptionInput.value;
+        const location = LocationInput.value;
+        const startDate = startdateInput.value;
+        const endDate = enddateInput.value;
+
+        
+
+
+
+
+
+    })
+
+
+
+    // End of all code
+})
